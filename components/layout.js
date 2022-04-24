35476cbc4396f17ef8530ebea65052146b3ff5d9
Austin White <austinlldb@gmail.com>
2022-04-23 22:52:24 -0600

    initial commit

---
+++ components/layout.js
@@ -0,0 +1,10 @@
+// import Head from 'next/head'
+// import Link from 'next/link'
+
+import TopBar from './topbar'
+
+export default function Layout() {
+  return (
+    <TopBar />
+  )
+}
---
+++ components/topbar.js
@@ -0,0 +1,5 @@
+export default function TopBar() {
+  return (
+    <h1>Hi there</h1>
+  )
+}
