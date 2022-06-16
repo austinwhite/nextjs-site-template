7aeab878ef7e096ab40af21a041a0069bff21e74
Austin White <austinlldb@gmail.com>
2022-06-15 22:57:35 -0600

    tagging system for blog

--- src/components/coming-soon.tsx
+++
@@ -1,9 +0,0 @@
-import comingSoonStyles from "@/styles/coming-soon.module.css"
-
-export default function ComingSoon({ text }) {
-  return (
-    <div className={ comingSoonStyles["container"] }>
-      <h1>{text} under development.</h1>
-    </div>
-  )
-}
