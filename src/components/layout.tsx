de440ba6b1fff168ef8105d24beaf254dfa01db7
Austin White <austinlldb@gmail.com>
2022-06-17 22:58:15 -0600

    fix build warnings/errors

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,6 +1,6 @@
 import TopNav from "./top-nav"
 
-export default function Layout({ children }) {
+export default function Layout({ children }: {children: JSX.Element}) {
   return (
     <div>
       <TopNav />
