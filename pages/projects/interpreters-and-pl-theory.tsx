73efaa297f2f998ee0e1e7ed4ddbdd10884badbb
Austin White <austinlldb@gmail.com>
2022-09-13 19:13:45 -0600

    projects page

--- pages/projects/interpreters-and-pl-theory.tsx
+++ pages/projects/interpreters-and-pl-theory.tsx
@@ -1,14 +1,27 @@
 import Head from 'next/head'
-import ContactBlock from "@/src/components/contact-block"
+import MlAlg from "@/src/components/ml-alg"
 import utilStyles from "@/styles/utils.module.css"
+import projStyles from "@/styles/projects.module.css"
 
 export default function Contact() {
   return (
     <div className={ utilStyles["page-container"] }>
       <Head>
-        <title>Contact</title>
+        <title>Projects</title>
       </Head>
-      <ContactBlock/>
+
+      <div className={ projStyles["ml-algs"] }>
+        <h1>Programming Language Theory and Interpreters</h1>
+        <br/>
+        <MlAlg 
+          name="jlox"
+          url="https://github.com/austinwhite/jlox" 
+        />
+        <MlAlg 
+          name="Scheme Interpreter"
+          url="https://github.com/austinwhite/scheme-interpreter" 
+        />
+      </div>
     </div>
   )
 }
